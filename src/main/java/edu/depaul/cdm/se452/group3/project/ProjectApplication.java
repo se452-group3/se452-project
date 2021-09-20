b2eff3b5d45f9bfe3693a53dbcfaf937c7e44f3a
sla-m1 <steven.la@datastax.com>
2021-09-19 20:53:38 -0700

    Springboot upload, with some readme info

---
+++ src/main/java/edu/depaul/cdm/se452/group3/project/ProjectApplication.java
@@ -0,0 +1,13 @@
+package edu.depaul.cdm.se452.group3.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
