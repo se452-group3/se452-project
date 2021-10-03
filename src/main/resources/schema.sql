a251cdc15ff00f29fb22aef3098e1635d578742b
Brandon Harrington <brandon1202h@gmail.com>
2021-10-03 18:00:48 -0400

    Added New Tables To schema.sql
    I added job_posting and profile

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,6 @@
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS profile CASCADE;
+DROP TABLE IF EXISTS job_posting CASCADE;
 
 create table users (
   id serial primary key,
@@ -7,4 +9,22 @@ create table users (
 );
 
 create table profile (
+  pid serial PRIMARY KEY,
+  uid integer,
+  bio varchar(500),
+  pictures integer, /*Placeholder need to talk with team about this*/
+  address varchar(60),
+  name varchar(60),
+  raiting integer,
+  review varchar(500), /*Also need to talk with the group about this one*/
+  FOREIGN KEY(uid) REFERENCES users(id)
+);
+
+create table job_posting (
+  ownerName varchar(60),
+  jobDescp varchar(500),
+  jobDate timestamp,
+  jobLocation varchar(60),
+  offers integer, /*Talk with the team about this one*/
+  PRIMARY KEY (jobDate,jobLocation)
 );
