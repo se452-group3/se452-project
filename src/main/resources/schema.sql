7bbd1fad21bb17ae6f98fc4002499800582b5e39
Steven La <steven.la@datastax.com>
2021-10-21 00:17:36 -0700

    Edits to User schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -13,8 +13,10 @@ DROP TABLE IF EXISTS DogParty       CASCADE;
 
 create table users (
   id serial primary key,
-  firstname varchar(20),
-  lastname varchar(20)
+  username varchar(20),
+  password varchar(20),
+  usertype varchar(20),
+  uuid varchar(36)
 );
 
 create table profile (
