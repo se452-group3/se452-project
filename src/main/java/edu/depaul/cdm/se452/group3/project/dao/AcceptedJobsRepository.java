c94df3d3e710b5bb08d22e9d44d56205c10d1a0c
Ana Vargas <anacisneros6211@gmail.com>
2021-10-26 19:59:47 -0500

    fixed acceptedjobs repo and reviews repo file name

--- src/main/java/edu/depaul/cdm/se452/group3/project/dao/AcceptedJobsRepository.java
+++ src/main/java/edu/depaul/cdm/se452/group3/project/dao/AcceptedJobsRepository.java
@@ -2,5 +2,5 @@ package edu.depaul.cdm.se452.group3.project.dao;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface AcceptedJobs extends CrudRepository<AcceptedJobs, Integer> {
+public interface AcceptedJobsRepository extends CrudRepository<AcceptedJobsRepository, Integer> {
 }
--- src/main/java/edu/depaul/cdm/se452/group3/project/dao/ReviewsRepository.java
+++ src/main/java/edu/depaul/cdm/se452/group3/project/dao/ReviewsRepository.java
@@ -2,5 +2,5 @@ package edu.depaul.cdm.se452.group3.project.dao;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface Reviews extends CrudRepository<Reviews, Integer>{
+public interface ReviewsRepository extends CrudRepository<ReviewsRepository, Integer>{
 }
