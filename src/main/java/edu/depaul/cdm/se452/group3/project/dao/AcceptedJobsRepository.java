aae0af4576d64a6c696ff67dbaaba5f3c5e93be8
Ana Vargas <anacisneros6211@gmail.com>
2021-10-26 20:09:36 -0500

    corrected parameters for jobposting, reviews in dao

--- src/main/java/edu/depaul/cdm/se452/group3/project/dao/AcceptedJobsRepository.java
+++ src/main/java/edu/depaul/cdm/se452/group3/project/dao/AcceptedJobsRepository.java
@@ -2,5 +2,5 @@ package edu.depaul.cdm.se452.group3.project.dao;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface AcceptedJobsRepository extends CrudRepository<AcceptedJobsRepository, Integer> {
+public interface AcceptedJobsRepository extends CrudRepository<AcceptedJobs, Integer> {
 }
