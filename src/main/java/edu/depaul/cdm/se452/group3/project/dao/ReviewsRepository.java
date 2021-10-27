7faf34ffd7e0704d195be9683ea251017278f674
Ana Vargas <anacisneros6211@gmail.com>
2021-10-26 20:11:13 -0500

    added import statements to jobposting repository, reviewsrepository

--- src/main/java/edu/depaul/cdm/se452/group3/project/dao/ReviewsRepository.java
+++ src/main/java/edu/depaul/cdm/se452/group3/project/dao/ReviewsRepository.java
@@ -1,5 +1,6 @@
 package edu.depaul.cdm.se452.group3.project.dao;
 
+import edu.depaul.cdm.se452.group3.project.entities.Reviews;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ReviewsRepository extends CrudRepository<Reviews, Integer>{
